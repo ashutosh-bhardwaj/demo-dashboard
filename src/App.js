8762df881485ce1aeec75239f8ba3376a43a705d
ashutosh bhardwaj <ashutosh.bhardwaj@daffodilsw.com>
2018-10-23 15:16:34 +0530

    Implement dynamic routing and sign-in design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
-
+import SignIn from './components/SignIn';
+import SimpleTable from './components/SimpleTable';
 class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={SimpleTable} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </React.Fragment>
     );
   }
 }
